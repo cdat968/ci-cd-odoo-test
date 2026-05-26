90e7d69e87c4ec0c154ee338139bb90992b490e3
NguyenChauQuocDat <cdat789@gmail.com>
2026-05-26 14:11:44 +0700

    test: break severity assertion to test CI fail flow

--- qa_bug_management/tests/test_qa_bug_ticket.py
+++ qa_bug_management/tests/test_qa_bug_ticket.py
@@ -36,7 +36,7 @@ class TestQaBugTicket(TransactionCase):
 
     def test_default_severity_medium(self):
         ticket = self.env['qa.bug.ticket'].create({'title': 'Default severity'})
-        self.assertEqual(ticket.severity, 'medium')
+        self.assertEqual(ticket.severity, 'critical')
 
     def test_default_status_new(self):
         ticket = self.env['qa.bug.ticket'].create({'title': 'Default status'})
