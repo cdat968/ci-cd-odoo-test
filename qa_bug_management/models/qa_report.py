ad087b578d942c0c1b9359c547747ad82f795456
NguyenChauQuocDat <cdat789@gmail.com>
2026-05-27 17:23:17 +0700

    trigger error

--- qa_bug_management/models/qa_report.py
+++ qa_bug_management/models/qa_report.py
@@ -1,3 +1,4 @@
+import os
 import secrets
 from odoo import models, fields, api
 
@@ -11,9 +12,18 @@ class QaReport(models.Model):
     project_name = fields.Char(string='Project')
     report_date = fields.Char(string='Report Date')
     share_token = fields.Char(string='Share Token', readonly=True, index=True, copy=False)
+    share_url = fields.Char(string='Share URL', compute='_compute_share_url')
     html = fields.Text(string='HTML Source')
     bug_ids = fields.One2many('qa.bug.ticket', 'report_id', string='Bug Tickets')
     bug_count = fields.Integer(string='Bug Count', compute='_compute_bug_count', store=True)
+
+    def _compute_share_url(self):
+        base = os.environ.get('BASE_WEBAPP_URL', '').rstrip('/')
+        for rec in self:
+            if base and rec.share_token:
+                rec.share_url = f'{base}/r/{rec.id}?t={rec.share_token}'
+            else:
+                rec.share_url = ''
 
     @api.depends('bug_ids')
     def _compute_bug_count(self):
